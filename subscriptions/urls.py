81f919ca8def8f2ec7881a1bce3b9bc440e0362c
mahmoudalkirm90 <mahmoudalkirm90@gmail.com>
2026-06-06 18:15:23 +0300

    Making the viewsets (publications, workshop)  in the project only int the src.urls, We need only DefaultRouter instance

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,10 +1,1 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import WorkshopViewSet
 
-router = DefaultRouter()
-router.register('', WorkshopViewSet, basename='workshops')
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
