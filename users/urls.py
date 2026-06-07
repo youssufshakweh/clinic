989bdeca1ba98cb862c5ebed404a94d4c15bed14
mahmoudalkirm90 <mahmoudalkirm90@gmail.com>
2026-06-07 23:17:49 +0300

    Fixing bug in urls, Viewsets should be in the src.urls only and remove the paths that caused infint loop in urls

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
+from django.urls import path
 
 from .views import VerifyEmailView
 
 urlpatterns = [
     path('users/verify_email/', VerifyEmailView.as_view(), name='verify-email'),
-    path('', include(router.urls)),
 ]
