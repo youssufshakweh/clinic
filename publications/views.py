8c4610ecaa9a1799a3a109ef72e3a45bf5af908a
Lamaizzaldeen <lamaizzaldeen@gmail.com>
2026-06-07 08:07:19 +0300

    feat: add cart, order, payments and fix otp

--- publications/views.py
+++ publications/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from .models import EducationalPublication
